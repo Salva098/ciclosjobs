f4aab9708bf531fc04a1f1ff0a3a703a051b6fcd
Salva098 <Salvadorgonzalezluque1@gmail.com>
2022-03-06 17:50:58 +0100

    Update Program.cs

--- ciclosjobs.API/Program.cs
+++ ciclosjobs.API/Program.cs
@@ -24,7 +24,7 @@ namespace ciclosjobs.API
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseUrls("https://*:5001", "http://*:5000");
+                    webBuilder.UseUrls("http://*:5001", "http://*:5000");
                     webBuilder.UseStartup<Startup>();
                 });
     }
