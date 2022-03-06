d8ec73feadc4cc6346dc2fd9ebf563aa96a3efa2
Salva098 <Salvadorgonzalezluque1@gmail.com>
2022-03-06 19:20:55 +0100

    Update Startup.cs

--- ciclosjobs.API/Startup.cs
+++ ciclosjobs.API/Startup.cs
@@ -135,6 +135,11 @@ namespace ciclosjobs.API
             app.UseAuthorization();
             app.UseAuthentication();
 
+            app.UseCors(x => x
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
